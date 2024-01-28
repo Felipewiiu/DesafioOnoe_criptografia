08df15fbebe9b2282e22fd7a97722d7aaff0a6bc
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2024-01-27 21:07:16 -0300

    feat: implementando a lógica de validação de dados

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const containerGroup = document.querySelector(".container_aside--img");
 const boxText = document.querySelector(".box-text");
 const copyBtn = document.querySelector(".copy-btn")
 const label = document.querySelector("#label")
+const texTip = document.querySelector(".text__tip")
 textImput.value = '';
 
 const objectCripity = {
@@ -15,7 +16,7 @@ const objectCripity = {
     'i': 'imes',
     'o': 'ober',
     'u': 'ufat'
-  };
+};
 
 const objectDeCripity = {
     'ai': 'a',
@@ -23,7 +24,7 @@ const objectDeCripity = {
     'imes': 'i',
     'ober': 'o',
     'ufat': 'u'
-  };
+};
 
 //Logica de habilitar e desabilitar a imagem lateral
 textImput.addEventListener("input", () => {
@@ -39,11 +40,33 @@ textImput.addEventListener("input", () => {
         copyBtn.classList.add("disable-content")
     }
 
+    const regex = new RegExp("^[a-z0-9\\s]+$", 'g')
+
+    const test = regex.test(boxText.value)
+
+
+    if (test) {
+        texTip.classList.remove("text-error")
+        console.log("Passou no teste no teste")
+        encryptBtn.removeAttribute("disabled")
+
+    } else {
+        console.log("Reprovou no teste")
+        texTip.classList.add("text-error")
+        encryptBtn.setAttribute("disabled", true)
+
+    }
+
+    boxText.value == "" ? 
+    texTip.classList.remove("text-error") &&
+     encryptBtn.removeAttribute("disabled") : "";
+
+
 });
 
 
 encryptBtn.addEventListener("click", () => {
-   cryptography(textImput, objectCripity, boxText)
+    cryptography(textImput, objectCripity, boxText)
 })
 
 
@@ -52,17 +75,17 @@ decryptBtn.addEventListener("click", () => {
     cryptography(textImput, objectDeCripity, boxText)
 })
 
-function desableLable(){
+function desableLable() {
     label.classList.add("label-disable")
 }
 
-copyBtn.addEventListener("click",() =>{
+copyBtn.addEventListener("click", () => {
     copy(boxText)
     label.classList.remove("label-disable")
     label.classList.add("label-enable")
     setTimeout(desableLable, 2000)
-    
-    
+
+
 })
 
 
